02a89a6a098b09603fb541dc44729ad778e6b742
jihyeb <jihyeb@bizwizsystem.co.kr>
2023-02-07 14:20:27 +0900

    [20230207] jihye B / Find Codename refactoring

--- Jihye_B/20230202/code.js
+++ Jihye_B/20230202/code.js
@@ -1,27 +1,19 @@
 const tableData = () => {
-  const data = a.data;
+  const data = basicData.data;
   //입력받은 데이터 숫자, 문자 구분하여 keyWord 추출
-  enterData = document.getElementById("search").value;
+  let enterData = document.getElementById("search").value;
   if (enterData) {
-    const findData = data.filter((row)=> {
-      if (enterData == row.code) {
-        return row.codeName;
-      } 
-      if (enterData == row.codeName) {
-        return row.codeName;
-      }
-  });
-    const keyWord = findData[0].codeName;
-    //키워드 사용하여 text 변경
-    if (keyWord) {
-      const newData = data.filter((row)=> {
-      return keyWord == row.codeName;
+    if (!isNaN(enterData)) { 
+      const findCode = data.find(row => row.code == enterData);
+      enterData = findCode.codeName;
+    }
+    const result = data.filter((row)=> {
+      return enterData == row.codeName;
     })
-    const result = newData.slice(0, 2)
     document.getElementById(result[0].code).innerText = 'find-'+ result[0].codeName
     document.getElementById(result[1].code).innerText = 'find-'+ result[1].codeName
   }
-}
+// }
   //초기 테이블 생성
   const setData = () => {
     for (i = 0; i < data.length; i++) {
@@ -41,5 +33,8 @@ const tableData = () => {
     set : setData
   }
 };
+
 const outData = tableData();
-outData.set();
+window.onload = () => {
+  outData.set();
+};
