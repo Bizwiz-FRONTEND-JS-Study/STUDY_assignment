c9962b4a54fc65dd3ff8a03eb4d2ec4d7f2c0b76
JEONG\hepat <hepatica387@gmail.com>
2023-02-20 14:08:35 +0900

    [20230220] seokjin Jung / calculator

--- JeongSeokJin/20230220/calculator.js
+++ JeongSeokJin/20230220/calculator.js
@@ -1,3 +1,5 @@
+import Calcultor from './example.js';
+
 const calcultor_div = document.querySelector('.calculator');
 const calculator_txt = document.querySelector('.calculator_txt');
 const calculator_content = calcultor_div.querySelector('.calculator_content');
@@ -19,59 +21,3 @@ calculator_content.addEventListener('click', function(e){
     calcultor.activeOperator();
   }
 })
-
-class Calcultor{
-  constructor(inputNum, value, operator){
-    this.inputNum = inputNum*1
-    this.value = value
-    this.operator = operator
-  }
-
-  insertValue(){
-    if(this.inputNum != 0){
-      calculator_txt.innerText += this.value
-      prevNum = calculator_txt.innerText;
-    }else{
-      calculator_txt.innerText = this.value
-    }
-    console.log(prevNum);
-  }
-
-  activeOperator(){
-    let result = 0;
-    
-    // console.log('prevNum',prevNum);
-    switch(this.operator){
-      case "+":
-        calculator_txt.innerText = 0;
-        break;
-      case "-":
-        break;
-      case "*":
-        break;
-      case "/":
-        break;
-      case "=":
-        result = this.inputNum + prevNum;
-        console.log("this.inputNum",this.inputNum);
-
-        console.log("prevNum",prevNum);
-
-        console.log("result",result);
-        // result = `${prevNum} ${prevOpt} ${calculator_txt.innerText*1}`;
-        calculator_txt.innerText = result
-        break;
-      case "C":
-        result = 0;
-        prevNum = 0;
-        calculator_txt.innerText = 0
-      break;
-      default:
-        break;
-      }
-    // calculator_txt.innerText = result
-  }
-  
-
-}
-
