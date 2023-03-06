cc021e29e8695656d42f30ab245440ca017424da
JEONG\hepat <hepatica387@gmail.com>
2023-03-06 10:42:08 +0900

    [20230306] MealApi

--- JeongSeokJin/20230220/calculator.js
+++ JeongSeokJin/20230220/calculator.js
@@ -1,23 +1,66 @@
-import Calcultor from './example.js';
+// import { Calculator } from "./example";
 
-const calcultor_div = document.querySelector('.calculator');
-const calculator_txt = document.querySelector('.calculator_txt');
-const calculator_content = calcultor_div.querySelector('.calculator_content');
-
-
-const result_array = []
-let prevNum = 0;
-
-calculator_content.addEventListener('click', function(e){
-  const inputNum = calculator_txt.innerText;
-  const value = e.target.value;
-  const operator = e.target.innerText;
-  
-  let calcultor = new Calcultor(inputNum, value, operator);
-
-  if(value !== ''){
-    calcultor.insertValue();
-  }else{
-    calcultor.activeOperator();
+class Calculator {
+  constructor() {
+    this.result = '0';
   }
-})
+
+  clear() {
+    this.result = '0';
+  }
+
+  append(value) {
+    if(this.result === "0" && value === "0"){
+      console.log(123);
+    }else if(this.result === "0"){
+      this.result = value.toString();
+    }else{
+      this.result += value.toString();
+    }
+  }
+
+  calculate() {
+    try {
+      this.result = eval(this.result).toString();
+    } catch (error) {
+      this.result = 'Error';
+    }
+  }
+
+  updateDisplay() {
+    const display = document.getElementById('result');
+    display.value = this.result;
+  }
+}
+
+const calculator = new Calculator();
+
+const numberButtons       = document.querySelectorAll('[data-number]');
+const operationButtons    = document.querySelectorAll('[data-operation]');
+const equalsButton        = document.querySelector('[data-equals]');
+const clearButton         = document.querySelector('[data-clear]');
+const display             = document.getElementById('result');
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.append(button.dataset.number);
+    calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.append(button.dataset.operation);
+    calculator.updateDisplay();
+  });
+});
+
+equalsButton.addEventListener('click', () => {
+  calculator.calculate();
+  calculator.updateDisplay();
+});
+
+clearButton.addEventListener('click', () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
